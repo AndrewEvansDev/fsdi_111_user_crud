6eb8d1280c73da146be9cc3bfac6eaf675f44da3
Andrew Evans <78834502+AndrewEvansDev@users.noreply.github.com>
2021-06-01 00:01:00 -0700

    initial submit

---
+++ app/routes.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""HTTP route definitions"""
+
+from flask import request
+from app import app # from the app package import the app variable
+from app.database import(
+    create,
+    scan,
+    delete_user
+)
+
+@app.route("/users", methods=["POST"]) # decorator
+def create_user():                      # view functions
+    user_data = request.json
+    new_id = create(
+        user_data.get("first_name"),
+        user_data.get("last_name"),
+        user_data.get("hobbies")
+    )
+    return{
+    "ok": True,
+    "message": "success",
+    "new_id": new_id
+    }
+
+
+@app.route("/users", methods=["GET"])
+def get_all_users():
+    users = scan()
+    return{
+        "ok": True,
+        "message": "Success",
+        "users": users
+    }
+
+
+
+@app.route("/users/delete/<int:user>", methods=["DELETE"])
+def delete(user):
+    delete_user(user)
+    return "%s was deleted." % user
+
+
+@app.route('/square/<int:number>')
+def square(number):
+    return ("<h1>%s squared is %s</h1>"
+            % (number, number**2))
+
+        
+@app.route('/agent')
+def agent():
+    user_agent = request.headers.get("User-Agent")
+    return "<p>Your user agent is %s</p>" % user_agent
